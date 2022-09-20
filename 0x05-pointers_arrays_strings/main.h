6faeb8a210924566263b7c86e2e7449a0d289cb4
ykeft52 <Robertcollins814@yahoo.com>
2022-09-19 19:14:20 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+ #ifndef MAIN_H
+ #define MAIN_H
+  /**
+ ** void prototypes()
+ * int prptotypes()
+ */
+
+ void reset_to_98(int *n);
+ void swap_int(int *a, int *b);
+ int_string(char *s);
+ void _puts(char *str);
+ void print_rev(char *s);
+ void rev_string(char *s);
+ void put%2(char *str);
+ void puts_half(char *str);
+ void print _array(int *a, int n);
+ char *_strcpy(char *dest, char *src);
+ int _atol(char *str);
+
+ #endif /*MAIN_H*/
